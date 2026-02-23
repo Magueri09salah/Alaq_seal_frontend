e16258f63ce37fe6ae4757048e3d61a6ba1253af
Magueri09salah <maguerisalaheddine@gmail.com>
2026-02-23 11:39:18 +0000

    google button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import toast from 'react-hot-toast';
-import GoogleButton from '../components/auth/GoogleButton';
+import GoogleButton from '../components/auth/Googlebutton';
 
 export default function LoginPage() {
   const navigate = useNavigate();
